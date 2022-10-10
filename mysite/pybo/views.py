22985304a01dc0a3e887ce331cca280ebbb973f7
jeheonee <shout_world@naver.com>
2022-10-10 18:27:36 +0900

    [DESIGN] login, signup page redesign

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -7,20 +7,22 @@
 #     return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
 
 
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'pybo/index.html')
 
-def Mypage(request): 
-    return render(request, 'pybo/Mypage.html') 
+@login_required(login_url='common:login')
+def Mypage(request):
+    return render(request, 'pybo/Mypage.html')
+
+@login_required(login_url='common:login')
+def PortfolioFeedback(request): 
+    return render(request, 'pybo/PortfolioFeedback.html')
 
 def Login(request): 
     return render(request, 'common/login.html')
-
-def PortfolioFeedback(request): 
-    return render(request, 'pybo/PortfolioFeedback.html')
 
 def KospiMarketMap(request): 
     return render(request, 'pybo/KospiMarketMap.html')
