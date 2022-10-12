b3021dbd526184bf39c789c698ed065baea6cbbd
Byeong Heon Lee <lww7438@gmail.com>
2022-10-12 15:41:12 +0900

    [DOCS] revise coding convention example code

--- documents/coding_convention_example.py
+++ documents/coding_convention_example.py
@@ -28,7 +28,7 @@ class Person(): # 클래스의 이름은 대분자로 시작, 가급적 한 음�
     사람을 모델링한 클래스이다.
     """
 
-    def __init__(self, prop1, prop2="abc"):
+    def __init__(self, prop1, prop2="abc"):  # 메서드의 이름은 동사로 시작, 각 음절 사이에는 언더바(_) 삽입, 함수 이름은 소문자로 구성, 함수의 리턴값은 타입만 기입
         """
         <메서드에 대한 개괄적인 설명> (~이다 체)
         Person 클래스의 생성자이다.
@@ -39,7 +39,7 @@ class Person(): # 클래스의 이름은 대분자로 시작, 가급적 한 음�
         prop2 (str)    : <해당 매개변수에 대한 간략한 설명> (default="abc")
 
         [Returns]
-        self (Person) : 생성된 클래스 인스턴스 
+        Person : 생성된 클래스 인스턴스 
         """
 
         self.prop1 = prop1
@@ -47,7 +47,7 @@ class Person(): # 클래스의 이름은 대분자로 시작, 가급적 한 음�
 
 
 # Function Declaration
-def get_stock_name(ticker:str):  # 함수의 이름은 동사로 시작, 각 음절 사이에는 언더바(_) 삽입, 함수 이름은 소문자로 구성
+def get_stock_name(ticker:str):  # 함수의 이름은 동사로 시작, 각 음절 사이에는 언더바(_) 삽입, 함수 이름은 소문자로 구성, 함수의 리턴값은 타입만 기입
     """
     <함수에 대한 개괄적인 설명> (~이다 체)
     종목명을 조회할 종목의 티커를 입력받아 종목명을 반환한다.
@@ -56,7 +56,7 @@ def get_stock_name(ticker:str):  # 함수의 이름은 동사로 시작, 각 음
     ticker (str) : 조회할 종목의 티커
 
     [Returns]
-    stock_name (str) : 종목 이름
+    str : 종목 이름
     """
 
     stock_name = example_func(ticker)  # 변수의 이름은 명사로 구성, 각 음절 사이에는 언더바(_) 삽입, 변수 이름은 소문자로 구성
