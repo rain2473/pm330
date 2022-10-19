b2f8322fe445f2a9b8dfdc4db23ecbb86d40c235
harry7435 <harry960629@naver.com>
2022-10-19 10:52:02 +0900

    [STYLE] coding_convention

--- news_crawling.py
+++ news_crawling.py
@@ -1,10 +1,28 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import time
-from pykrx import stock
+# Author  : 조익준          
+# Contact : harry960629@naver.com      
+# Date    : 2022-10-19(수)
 
+# Required Modules  (from, import, as 구문들의 Indentation을 맞춘다.)
+import requests
+import time
+import pandas as pd
+
+from bs4    import BeautifulSoup
+from pykrx  import stock
+
+# Function Declaration
 def get_data(stock_ticker, page):
+    """
+    종목티커와 페이지 번호를 받아서 해당 종목의 네이버 금융 종목 뉴스 페이지의 헤드라인과 게시날짜 및 시간을 데이터프레임으로 반환한다.
+    
+    [Parameters]
+    stock_ticker : 종목의 티커
+    page : 뉴스 페이지 번호
+
+    [Returns]
+    df : 헤드라인, 기사날짜, 티커가 포함된 데이터프레임
+    """
+
     # 뉴스 n page url 설정
     url = f'https://finance.naver.com/item/news_news.naver?code={stock_ticker}&page={page}&sm=title_entity_id.basic&clusterId='
     
@@ -24,7 +42,7 @@ def get_data(stock_ticker, page):
         date_list.append(frame_items.select_one('span.tah').get_text())
     
     # 과정 확인 메시지 출력
-    # print(f'{stock_ticker} : {page} page 완료')
+    print(f'{stock_ticker} : {page} page 완료')
     
     # 데이터프레임으로 합치기  
     df = pd.concat([pd.DataFrame({'날짜':date_list}), pd.DataFrame({'뉴스제목':head_list})],axis=1)
@@ -33,8 +51,17 @@ def get_data(stock_ticker, page):
     
     return df
 
-# 뉴스 마지막 페이지 숫자 구하기
 def get_last(stock_ticker):
+    """
+    종목티커를 받아 해당 종목의 뉴스의 마지막 페이지 번호를 반환한다.
+    
+    [Parameters]
+    stock_ticker : 종목의 티커
+
+    [Returns]
+    last_number : 뉴스 마지막 페이지 번호
+    """
+
     url = f'https://finance.naver.com/item/news_news.naver?code={stock_ticker}&page=&sm=title_entity_id.basic&clusterId='
     res = requests.get(url)
     soup = BeautifulSoup(res.content, 'html.parser')
@@ -43,25 +70,29 @@ def get_last(stock_ticker):
     
     return last_number
 
+
+# Constants
 # 티커 리스트(코스피 200) 사용 시 주석 해제
-# ticker_list = stock.get_index_portfolio_deposit_file("1028")
+# TICKER_LIST = stock.get_index_portfolio_deposit_file("1028")
 
 # 데이터 추출
-for tick in ticker_list:
+for tick in TICKER_LIST:
     news_data = pd.DataFrame()
     try:
         start, end = 1, get_last(tick)
-    except Exception as e:
+    except Exception as e:  # 뉴스가 1페이지 밖에 없을 경우
         print(e)
         start, end = 1, 1
+
     print(start, end)
     now_page = start
+
     while now_page <= end:
         try:
             news_data = pd.concat([news_data, get_data(tick, now_page)], ignore_index=True)
             now_page += 1
             time.sleep(2)
-        except Exception as e:
+        except Exception as e:  # 로딩 오류 발생 시 재실행
             print(e)
 
     # 종목 뉴스 검색 종료 후 저장
