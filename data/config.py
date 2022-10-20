0e9d64ae5340d2b4a003d599f44f64f50535248e
Byeong Heon Lee <lww7438@gmail.com>
2022-10-20 17:17:01 +0900

    [FIX] hiding credentials

---
+++ data/config.py
@@ -0,0 +1,17 @@
+
+# Author      : 이병헌
+# Contact     : lww7438@gmail.com
+# Date        : 2022-10-19(수)
+
+
+
+# * * *   API Keys   * * *
+API_KEY_OPEN_DATA_PORTAL = '<공공데이터 포털 API Key>'
+
+# * * *   DB Connection Information   * * *
+POSTGRES_HOST     = 'pm330.c68tjqdjajqc.us-east-1.rds.amazonaws.com'
+POSTGRES_PORT     = 5432   # Default port number of postgres
+POSTGRES_USER     = 'postgres'
+POSTGRES_USER_PW  = '<USER 비밀번호>'
+POSTGRES_ADMIN    = 'byeong_heon'
+POSTGRES_ADMIN_PW = '<DBA 비밀번호>'
