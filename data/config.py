82b531f9fb56ceb2f69bd7986e18ae0b84a0c5a2
Byeong Heon Lee <lww7438@gmail.com>
2022-10-21 11:46:39 +0900

    [FIX] add default database name

--- data/config.py
+++ data/config.py
@@ -12,20 +12,26 @@ API_KEY_OPEN_DATA_PORTAL = '<공공데이터 포털 API Key>'
 POSTGRES_HOST     = 'pm330.c68tjqdjajqc.us-east-1.rds.amazonaws.com'
 POSTGRES_PORT     = 5432   # Default port number of postgres
 
+# 이병헌
 ID_DBA = 'byeong_heon'
 PW_DBA = '<password of DBA>'
 
+# 정제헌
 ID_JH = 'je_heon'
 PW_JH = 'a1234567b'
 
+# 홍윤기
 ID_YK = 'yoon_ki'
 PW_YK = 'hyk1234567'
 
+# 김덕룡
 ID_DR = 'deok_ryong'
 PW_DR = 'kdr1234567'
 
+# 유상균
 ID_SG = 'sang_gyun'
 PW_SG = 'Sg!57608'
 
+# 조익준
 ID_IJ = 'ik_jun'
 PW_IJ = '33547435'
