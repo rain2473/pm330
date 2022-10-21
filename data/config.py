0a5f055649ce8cc6e9f32fd308ab6713279f885c
Byeong Heon Lee <lww7438@gmail.com>
2022-10-21 11:01:01 +0900

    [FEAT] add IDs and Passwords of database users (Temporary)

--- data/config.py
+++ data/config.py
@@ -1,7 +1,7 @@
 
 # Author      : 이병헌
 # Contact     : lww7438@gmail.com
-# Date        : 2022-10-19(수)
+# Date        : 2022-10-21(금)
 
 
 
@@ -11,7 +11,21 @@ API_KEY_OPEN_DATA_PORTAL = '<공공데이터 포털 API Key>'
 # * * *   DB Connection Information   * * *
 POSTGRES_HOST     = 'pm330.c68tjqdjajqc.us-east-1.rds.amazonaws.com'
 POSTGRES_PORT     = 5432   # Default port number of postgres
-POSTGRES_USER     = 'postgres'
-POSTGRES_USER_PW  = '<USER 비밀번호>'
-POSTGRES_ADMIN    = 'byeong_heon'
-POSTGRES_ADMIN_PW = '<DBA 비밀번호>'
+
+ID_DBA = 'byeong_heon'
+PW_DBA = '<password of DBA>'
+
+ID_JH = 'je_heon'
+PW_JH = 'a1234567b'
+
+ID_YK = 'yoon_ki'
+PW_YK = 'hyk1234567'
+
+ID_DR = 'deok_ryong'
+PW_DR = 'kdr1234567'
+
+ID_SG = 'sang_gyun'
+PW_SG = 'Sg!57608'
+
+ID_IJ = 'ik_jun'
+PW_IJ = '33547435'
