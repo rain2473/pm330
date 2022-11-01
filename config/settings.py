141a671b5b9b68a6a37b837fbc019a01c76216de
Byeong Heon Lee <lww7438@gmail.com>
2022-11-01 16:54:29 +0900

    [CHORE] fix to deploy

--- config/settings.py
+++ config/settings.py
@@ -33,8 +33,8 @@ SECRET_KEY = 'django-insecure-=$v@*ijymynv$0j_88#4#y6do%zev99qf+*m*6mm2zglm+3m*a
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
+# DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
